b4d58db4a3a4984c916d6b4c48c5f6a0455c82fd
Laxy2211 <160948850+Laxy2211@users.noreply.github.com>
2026-01-10 16:51:21 -0500

    new ingredients items added

---
+++ src/main/java/net/cata/katalya/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.cata.katalya.block;
+
+import net.cata.katalya.Katalya;
+
+public class ModBlocks {
+    public static void registerModBlocks(){
+        Katalya.LOGGER.info("Registering");
+    }
+}
--- src/main/java/net/cata/katalya/items/ModItems.java
+++ src/main/java/net/cata/katalya/items/ModItems.java
@@ -3,7 +3,6 @@ package net.cata.katalya.items;
 import net.cata.katalya.Katalya;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -14,6 +13,26 @@ public class ModItems {
     public static final Item LUMIA = registerItem("lumia",new Item(new Item.Settings()));
     public static final Item LIFE_SOUL = registerItem("life_soul",new Item(new Item.Settings()));
     public static final Item TERMIA = registerItem("termia",new Item(new Item.Settings()));
+    public static final Item LIGHT_FEATHER = registerItem("light_feather",new Item(new Item.Settings()));
+    public static final Item REINFORCED_FEATHER = registerItem("reinforced_feather",new Item(new Item.Settings()));
+    public static final Item SHADOW_SOUL = registerItem("shadow_soul",new Item(new Item.Settings()));
+    public static final Item DARK_SOUL = registerItem("dark_soul",new Item(new Item.Settings()));
+    public static final Item TORMENTED_BROKEN_BOW = registerItem("tormented_broken_bow",new Item(new Item.Settings()));
+    public static final Item TORMENTED_SOUL = registerItem("tormented_soul",new Item(new Item.Settings()));
+    public static final Item SKULL = registerItem("skull",new Item(new Item.Settings()));
+    public static final Item THORN = registerItem("thorn",new Item(new Item.Settings()));
+    public static final Item CLOROFILE = registerItem("clorofile",new Item(new Item.Settings()));
+    public static final Item SCALE = registerItem("scale",new Item(new Item.Settings()));
+    public static final Item CORRUPTED_SHEET = registerItem("corrupted_sheet",new Item(new Item.Settings()));
+    public static final Item SILK = registerItem("silk",new Item(new Item.Settings()));
+    public static final Item BETLEE_SHELL = registerItem("betlee_shell",new Item(new Item.Settings()));
+    public static final Item WING_FRAGMENT = registerItem("wing_fragment",new Item(new Item.Settings()));
+    public static final Item STING = registerItem("sting",new Item(new Item.Settings()));
+    public static final Item WORKER_LEGS = registerItem("worker_legs",new Item(new Item.Settings()));
+    public static final Item SHINY_SHELL = registerItem("shiny_shell",new Item(new Item.Settings()));
+    public static final Item RETRACTABLE_BEAK = registerItem("retractable_beak",new Item(new Item.Settings()));
+    public static final Item ARACHNID_SCALE = registerItem("arachnid_scale",new Item(new Item.Settings()));
+    public static final Item GELID_EYE = registerItem("gelid_eye",new Item(new Item.Settings()));
    private static Item registerItem(String name, Item item){
        return Registry.register(Registries.ITEM, Identifier.of(Katalya.MOD_ID, name), item);
    }
@@ -25,6 +44,27 @@ public class ModItems {
             entries.add(LUMIA);
             entries.add(LIFE_SOUL);
             entries.add(TERMIA);
+            entries.add(LIGHT_FEATHER);
+            entries.add(REINFORCED_FEATHER);
+            entries.add(SHADOW_SOUL);
+            entries.add(DARK_SOUL);
+            entries.add(TORMENTED_BROKEN_BOW);
+            entries.add(TORMENTED_SOUL);
+            entries.add(SKULL);
+            entries.add(THORN);
+            entries.add(CLOROFILE);
+            entries.add(SCALE);
+            entries.add(CORRUPTED_SHEET);
+            entries.add(SILK);
+            entries.add(BETLEE_SHELL);
+            entries.add(WING_FRAGMENT);
+            entries.add(STING);
+            entries.add(WORKER_LEGS);
+            entries.add(SHINY_SHELL);
+            entries.add(RETRACTABLE_BEAK);
+            entries.add(ARACHNID_SCALE);
+            entries.add(GELID_EYE);
+
         });
     }
 }
